486ccf6b91014d46c5b4de836fb46158413cb7d3
ChristianKS <ChristianKS@protonmail.com>
2022-12-16 16:53:46 -0300

    Rewrote nvim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,9 @@
+require("chrisks.remap")
+require("chrisks.set")
+require("autoclose").setup({})
+require('Comment').setup()
+
+vim.cmd([[
+	colorscheme PaperColor
+ 	hi normal guibg=NONE ctermbg=NONE
+]])
--- .config/nvim/lua/scroll.lua
+++
@@ -1,9 +0,0 @@
-require('cinnamon').setup {
-  default_keymaps = true,
-  extended_keymaps = false,
-  max_length = 500,
-  scroll_limit = -1,
-  
-  
-}
-
