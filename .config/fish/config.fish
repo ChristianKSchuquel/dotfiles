950f14cadbc21ecaafbed12fbf72bf55c03df764
ChristianKS <christianks@protonmail.com>
2023-03-21 11:08:53 -0300

    init new install

---
+++ .config/fish/config.fish
@@ -0,0 +1,30 @@
+﻿if status is-interactive
+# Commands to run in interactive sessions can go here
+    alias "config"='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
+    setxkbmap -layout us,br
+    setxkbmap -option 'grp:alt_shift_toggle'
+    
+    neofetch
+
+    echo -n Setting abbreviations... 
+
+    abbr cls 'clear'
+    abbr ls 'lsd --oneline --group-dirs=first'
+    abbr y 'yay'
+    abbr pS 'sudo pacman -S'
+    abbr pR 'sudo pacman -R'
+    abbr pU 'sudo pacman -Syu'
+    abbr bt 'bashtop'
+    abbr lg 'lazygit'
+    abbr grm 'go run main.go'
+    abbr cr 'cargo run'
+    abbr i3config '~/.config/i3; nvim .'
+    abbr nvconfig '~/.config/nvim; nvim .'
+    abbr fshconfig '~/.config/fish; nvim .'
+    abbr oSn 'optimus-manager --switch nvidia --no-confirm' 
+    abbr nv 'nvim .'
+    abbr hdmioff 'xrandr --output HDMI-0 --off'
+    abbr hdmion 'xrandr --output HDMI-0 --auto --right-of eDP-1-1'
+
+    echo 'Done'
+end
---
+++ .config/nvim/init.lua
@@ -0,0 +1,10 @@
+require("chrisks.remap")
+require("chrisks.set")
+require("autoclose").setup({})
+require('Comment').setup()
+
+vim.cmd([[
+	colorscheme material
+ 	hi normal guibg=NONE ctermbg=NONE
+]])
+
